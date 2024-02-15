c3129f4de92e32dad565bc43e5a49c06ba12ddab
Ilias <ilias.kadyrkulov@gmail.com>
2024-02-15 23:23:49 +0600

    fix: project value error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,11 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['jsx-a11y', '@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/strict',
-    'plugin:jsx-a11y/strict',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/strict', 'plugin:jsx-a11y/strict'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   rules: {
-    // ... Другие правила ...
     'jsx-a11y/anchor-is-valid': 'off',
   },
 }
